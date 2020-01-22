795e8c8f17dedcd0c992ffdd9d2589b4e8771f5c
Stefan Nychka <stefannychka@gmail.com>
2020-01-22 14:32:45 -0700

    done proj, added onto module3-solution

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 #    path('user/<int:id>', views.user_posts, name='user_posts'),
 #    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
     path('', views.index, name='index'),
+    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
 ] 
